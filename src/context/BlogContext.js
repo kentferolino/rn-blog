4aa18b92f58affd20ddc599eb79455d2a8f3d885
Kent Ferolino <kferolino@mersoft.com>
2019-10-08 01:15:33 +0800

    Initial commit. List of blog posts. Add blog post. Context setup.

---
+++ src/context/BlogContext.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const blogPosts1 = [{ title: "Blog Post 1" }, { title: "Blog Post 2" }];
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` }
+    ]);
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
